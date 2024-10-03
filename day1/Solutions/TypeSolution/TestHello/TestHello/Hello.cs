453c2a7a2ede65ac2c06ce989767873183cefc04
74717sarang <patilsarang13520@gmail.com>
2024-10-03 16:55:40 +0530

    3-10comit

---
+++ day1/Solutions/TypeSolution/TestHello/TestHello/Hello.cs
@@ -0,0 +1,14 @@
+using System;
+
+
+
+  console.WriteLine("hello");
+
+       console.WriteLine("hello");
+  console.WriteLine("Enter name:");
+ string s= console.ReadLine();
+
+   console.WriteLine("Hello"+name);
+
+
+
---
+++ day1/Solutions/TypeSolution/TestHello/TestHello/test.cs
@@ -0,0 +1,22 @@
+using  System;
+ 
+
+namespace test{
+
+     public class Test{
+
+       private int id;
+       private string name;
+       private int mark;
+
+
+       
+    
+
+
+     }
+
+
+
+
+}
