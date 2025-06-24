29176784fd2cb52949933e01a0cb758fc890c9c9
74717sarang <patilsarang13520@gmail.com>
2025-06-24 18:24:15 +0530

    string and array update

---
+++ Day5/ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class1
+    {
+
+        public Class1() {
+            Console.WriteLine("HIi by");
+        }
+
+
+        public static int large(int[] arr)
+        {
+            int l = arr[0];
+            int secl = arr[0];
+
+            int min=arr[0];
+            int secMin=arr[0];
+            
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if(arr[i] > l)
+                {
+                    secl = l;
+                    l= arr[i];  
+                }
+                else if(arr[i] > secl && arr[i] < l)
+                {
+                    secl= arr[i];
+                }
+
+
+                else if (arr[i] < min)
+                {
+                    secMin = min;
+                    min= arr[i];
+                }
+                else if(arr[i] < secMin && arr[i] > min)
+                {
+                    secMin= arr[i];
+                }
+            }
+            return secMin;
+        }
+
+
+
+        
+    }
+}
---
+++ Day5/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+Console.WriteLine("Hello, World!");
+
+
+
+int[] arr = { 1,2,3,4,5,6,7};
+
+
+Console.WriteLine(arr[2]);
+
+
+Console.WriteLine(Class1.large(arr));
