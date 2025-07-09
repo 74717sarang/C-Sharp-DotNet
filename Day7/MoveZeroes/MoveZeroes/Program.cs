c25ad7448a2fa3d5ae0c43928d7006aa434c563a
74717sarang <patilsarang13520@gmail.com>
2025-07-09 14:04:57 +0530

    add new day 7

---
+++ Day7/MoveZeroes/MoveZeroes/Program.cs
@@ -0,0 +1,35 @@
+﻿using MoveZeroes;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        int[] arr = { 9, 8, 7, 0, 5, 0, 3, 0 };
+        //		Arrays.sort(arr);
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write(arr[i] + " ");
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("leftShiift");
+        Zeroes.leftMoveZeros(arr);
+        Console.WriteLine();
+        Console.WriteLine("Right shift");
+
+        Zeroes.rightshift(arr);
+
+
+
+
+
+
+    }
+
+}
+
+
+
---
+++ Day7/MoveZeroes/MoveZeroes/Zeroes.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoveZeroes
+{
+    public class Zeroes
+    {
+
+        public static void leftMoveZeros(int[] arr)
+        {
+            int len=arr.Length;
+
+            for (int i = arr.Length-1; i >=0; i--) {
+
+                if (arr[i] != 0)
+                {
+                    arr[--len]=arr[i];
+                }
+            
+            }
+            while (len > 0)
+            {
+               arr[--len]=0;
+            }
+            //Console.WriteLine("Hii");
+
+
+            foreach(var i in arr)
+            {
+                Console.Write(i+" ");
+            }
+        }
+
+        internal static void rightshift(int[] arr)
+        {
+            int len = 0;
+            for (int i = 0; i < arr.Length; i++) {
+                if (arr[i] != 0)
+                {
+                    arr[len++]=arr[i];
+                }
+            }
+
+            while (len < arr.Length)
+            {
+                arr[len++] = 0;
+
+            }
+
+            foreach(int i in arr)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
